d2e1ceec3981e9719f66a711acc1e88883eead58
Hassan Khalid <hassan@createk.io>
2017-07-28 16:57:25 +0100

    no need to require_relative as bundler should require this using the require_paths in the gemspec

--- lib/relsr.rb
+++ lib/relsr.rb
@@ -1,4 +1,4 @@
-require_relative 'relsr/initializer'
-require_relative 'relsr/release_manager'
-require_relative 'relsr/version'
+require 'relsr/initializer'
+require 'relsr/release_manager'
+require 'relsr/version'
 
